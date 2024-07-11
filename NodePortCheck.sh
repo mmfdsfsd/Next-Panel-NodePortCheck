c3271a5ba915e1bc67a34e91dd5a9abebec7eda6
mmfdsfsd <99636892+mmfdsfsd@users.noreply.github.com>
2024-07-11 23:59:39 +0800

    Update NodePortCheck.sh

--- NodePortCheck.sh
+++ NodePortCheck.sh
@@ -16,16 +16,16 @@ do
     port=`echo $line | awk 'BEGIN{FS="|"} {print $2}'`
     echo "(sleep 1;) | telnet $domain $port"
     (sleep 1;) | telnet $domain $port > telnet_domain_result.txt
-    result=`cat telnet_node_result.txt | grep -B 1 \] | grep [0-9] | awk '{print $3}' | cut -d '.' -f 1,2,3,4`
+    result=`cat telnet_domain_result.txt | grep -B 1 \] | grep [0-9] | awk '{print $3}' | cut -d '.' -f 1,2,3,4`
     echo "$result"
     if [ -n "$result" ]; then
-        echo "$domain|$port|" >> telnet_node_checked.txt
+        echo "$domain|$port|" >> telnet_node_pre-checked.txt
 		
     else
 	
 		newport=`expr $port + 1`
         	echo $(date +"%y-%m-%d %H:%M:%S") "$domain:$port Fail! " >> telnet_domain_failed.txt
-		echo "$domain|$newport|" >> telnet_node_checked.txt
+		echo "$domain|$newport|" >> telnet_node_pre-checked.txt
 		
 		if [ "$domain" = "test.com" ]; then
 		update_sql="update ${TABLENAME} set custom_config = JSON_REPLACE(custom_config,'$.offset_port_node','$newport') where id =1"
@@ -36,5 +36,5 @@ do
 		
     fi
 done
-	mv  telnet_node_checked.txt  nodelist.txt
+	mv  telnet_node_pre-checked.txt  nodelist.txt
 	echo "telnet node_port checked over!"
